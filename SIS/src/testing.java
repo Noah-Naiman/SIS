17367f0fb15d1d8730a3132b60bd4526012a0954
noah.naiman <noah.naiman@MU-C1-12.mullen.pvt.k12.co.us>
2024-03-05 09:12:26 -0700

    cheese

---
+++ SIS/src/testing.java
@@ -0,0 +1,15 @@
+
+public class testing {
+
+	public static void main(String[] args) {
+		boolean jonathan = true;
+		while(jonathan)
+			{
+			System.out.println("Cheese");
+			
+			}
+		
+
+	}
+
+}
