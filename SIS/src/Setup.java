34c377e301cc5614fe954141d026ee16c8191292
fiona.callahan <fiona.callahan@MU-C1-01.mullen.pvt.k12.co.us>
2024-03-07 09:05:27 -0700

    started pojo

---
+++ SIS/src/Setup.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private double gpa;
+	private int course1;
+	private int course2;
+	private int course3;
+	private String course1Name;
+	private String course2Name;
+	private String course3Name;
+	private String course1Letter;
+	private String course2Letter;
+	private String course3Letter;
+	public Student (String f, String l, double g, int c1, int c2, int c3, String c1N, String c2N, String c3N, String c1L, String c2L, String c3L)
+	{
+		firstName = f;
+		lastName = l;
+		gpa = g;
+		course1 = c1;
+		course2 = c2;
+		course3 = c3;
+		course1Name = c1N;
+		course2Name = c2N;
+		course3Name = c3N;
+		course1Letter = c1L;
+		course2Letter = c2L;
+		course3Letter = c3L;
+	}
+	public String firstName getFirstName()
+	
+	
+
+	
+}
