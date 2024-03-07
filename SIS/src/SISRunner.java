2d15b666e03ce893bb76af39131293d4cfdfafb4
fiona.callahan <fiona.callahan@MU-C1-01.mullen.pvt.k12.co.us>
2024-03-07 09:18:02 -0700

    finish getters and setters

--- SIS/src/SISRunner.java
+++ SIS/src/SISRunner.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class SISRunner {
+	static Scanner userLongInput = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World!");
-		System.out.println("Good Job!");
+		System.out.println("What would you like to do in the student data base? ");
+		System.out.println("a) add or delete a student");
+		System.out.println("b) change student grades/schedule");
+		System.out.println("c) sort students");
+		 
 
 	}
 
