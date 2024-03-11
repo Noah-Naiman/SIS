646418f57490c0329def8f1472a909b9a6c2240f
Christopher-McBroom <ctmcbroom@comcast.net>
2024-03-11 08:44:10 -0600

    started working on Changing class but can't Syso for some reason.

--- SIS/src/Changing.java
+++ SIS/src/Changing.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class Changing {
-	Scanner myObj = new Scanner(System.in); 
-    //System.out.println("test");
+	
+	Scanner userIntInput = new Scanner(System.in);
+	System.out.println("What do you want to do?");
+	System.out.println("[1] Change a students grade");
+	System.out.println("[2] Change a students grade");
+	int userChoice = userIntInput.nextInt();
+	
 }
--- SIS/src/testing.java
+++ SIS/src/testing.java
@@ -2,13 +2,13 @@
 public class testing {
 
 	public static void main(String[] args) {
-		boolean jonathan = true;
-		while(jonathan)
-			{
-			System.out.println("Cheese");
-			
-			}
-		
+//		boolean jonathan = true;
+//		while(jonathan)
+//			{
+//			System.out.println("Cheese");
+//			
+//			}
+//		
 
 	}
 
