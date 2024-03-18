17da404f4a87d3dd821840696ff5aef563020b4c
noah.naiman <noah.naiman@MU-C1-12.mullen.pvt.k12.co.us>
2024-03-18 08:10:08 -0600

    test commit

--- SIS/src/Database.java
+++ SIS/src/Database.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
--- SIS/src/Sorting.java
+++ SIS/src/Sorting.java
@@ -2,7 +2,13 @@ public class Sorting
 {
 	public static void sortLastName()
 	{
-		
+		for(int i = 0; i < Student.database.length-1; i++)
+		{
+			if(Student.database.getLastName(i).substring(0,1).equals(Student.database.getLastName(i+1).substring(0,1)))
+			{
+				
+			}
+		}
 	}
 	
 	public static void sortGPA()
